7e30015ee37123485b8f8e22881e53495c052b6a
Blagopoluchnaya <blagopoluchnaya@email.com>
2024-02-01 13:53:59 +0300

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница сайта - HOME'
+        'title': 'Mining career - Главная',
+        'content': 'Выпускники Горного Университета'
     }
     
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Mining career - О нас',
+        'content': 'О нас',
+        'text_on_page': 'ldfkgvjaofslfnvskejfvnksdjfvn' 
+        
+    }
+    
+    return render(request, 'main/about.html', context)
