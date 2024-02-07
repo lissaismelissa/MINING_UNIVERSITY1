c487802136a3d84a9d6efff400127947ec3bbbd5
Blagopoluchnaya <blagopoluchnaya@email.com>
2024-02-07 10:39:05 +0300

    full text search

--- graduates/utils.py
+++ graduates/utils.py
@@ -1,20 +1,25 @@
 from django.db.models import Q
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from graduates.models import Graduates
 
 
 def q_search(query):
     if query.isdigit():
         return Graduates.objects.filter(id=int(query))
+    
+    vector = SearchVector("name", "current_work_or_study_place", "faculty__name", "direction__name", "profile__name", "qualification__name", "edication_form__name", "group__name", "department__name")
+    query = SearchQuery(query)
+    
+    return Graduates.objects.annotate(rank=SearchRank(vetcor, query)).order_by("-rank")
 
-    keywords = [word for word in query.split() if len(word) > 2]
+    # keywords = [word for word in query.split() if len(word) > 2]
 
-    q_objects = Q()
+    # q_objects = Q()
 
-    for token in keywords:
-        q_objects |= Q(name__icontains=token)
-        q_objects |= Q(current_work_or_study_place__icontains=token)
-        q_objects |= Q(graduation_year__icontains=token)
+    # for token in keywords:
+    #     q_objects |= Q(name__icontains=token)
+    #     q_objects |= Q(current_work_or_study_place__icontains=token)
+    #     q_objects |= Q(graduation_year__icontains=token)
 
 
-    return Graduates.objects.filter(q_objects)
+    # return Graduates.objects.filter(q_objects)
